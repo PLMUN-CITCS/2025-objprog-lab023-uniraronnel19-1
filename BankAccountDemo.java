d9723047b663985a74178d49a3f7a07e9409948d
Ronnel Unira <uniraronnel_bsit@plmun.edu.ph>
2025-02-28 10:02:22 -0700

    Submitting OBJPROG Week 05 - Laboratory # 23

--- BankAccountDemo.java
+++ BankAccountDemo.java
@@ -1,3 +1,15 @@
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount("Alice", 1000.0, "Checking");
+        
+        myAccount.deposit(250.0);
+        myAccount.withdraw(100.0);
+        System.out.println("Account Holder: " + myAccount.accountHolder);
+        System.out.println("Account Type: " + myAccount.accountType);
+        System.out.println("Current Balance: $" + myAccount.getBalance());
+    }
+}
+
 class BankAccount {
     public String accountHolder;
     private double balance;
@@ -23,17 +35,5 @@ class BankAccount {
     
     public double getBalance() {
         return balance;
-    }
-}
-
-public class BankAccountDemo {
-    public static void main(String[] args) {
-        BankAccount myAccount = new BankAccount("Alice", 1000.0, "Checking");
-        
-        myAccount.deposit(250.0);
-        myAccount.withdraw(100.0);
-        System.out.println("Account Holder: " + myAccount.accountHolder);
-        System.out.println("Account Type: " + myAccount.accountType);
-        System.out.println("Current Balance: $" + myAccount.getBalance());
     }
 }
